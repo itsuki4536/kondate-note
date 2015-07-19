418f358eb5f52f78bb8d9b3f6176a4861675e586
c-bata <contact@c-bata.link>
2015-07-19 16:32:14 +0900

    POSTでデータを送ると登録

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from bottle import route, run, template
+from bottle import route, post, run, template, request, redirect
 import db
 
 @route('/')
@@ -9,6 +9,15 @@ def kondate():
 @route('/menus/new')
 def kondate_new():
     return template('kondate_new')
+
+@post('/menus/new')
+def kondate_create():
+    name = request.forms.get('name')
+    kcal = request.forms.get('kcal')
+    image = request.forms.get('image')
+    import pdb; pdb.set_trace()
+    db.menus.insert().execute(name=name, kcal=kcal, image=image)
+    return redirect('/')
 
 
 run(host='localhost', port=8080, debug=True, reloader=True)
--- views/kondate_new.tpl
+++ views/kondate_new.tpl
@@ -7,9 +7,11 @@
 <body>
     <p>献立の追加</p>
     <form method="post" action="/menus/new">
+        <input type="hidden" name="mcode" value="UTF-8">
         名前：　<input type="text" name="name">
         カロリー：　<input type="text" name="kcal">
         画像URL：　<input type="text" name="image">
+        <button type="submit">登録</button>
     </form>
 </body>
 </html>
